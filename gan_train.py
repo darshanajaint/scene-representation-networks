gradient bug fix

gan_train.py
@@ -159,14 +159,14 @@ def set_up_discriminator(device):
 
 def checkpoint(models_dir, results_dir, iter, disc, disc_results, gen,
                gen_loss):
-    models_path = os.path.join(models_dir, 'iter_%06d.pth' % iter)
+    models_path = models_dir + '/iter_{:06d}.pth'.format(iter)
     models = {
         'discriminator': disc.model.state_dict(),
         'generator': gen.state_dict()
     }
     torch.save(models, models_path)
 
-    results_path = os.path.join(results_dir, 'iter_%6d.pth' % iter)
+    results_path = results_dir + '/iter_{:06d}.pth'.format(iter)
     results = {
         'discriminator': disc_results,
         'generator': gen_loss

srns.py
@@ -137,9 +137,11 @@ class SRNsModel(nn.Module):
         """
         Computes GAN loss on this model.
         """
-        m = float(len(fakes))
-        minus_pred = [1 - pred for pred in self.discriminator.predict(fakes)]
-        loss = 1 / m * torch.sum(torch.log2(torch.Tensor(minus_pred)))
+        fakes = np.concatenate(fakes)
+        m = len(fakes)
+        ones = torch.from_numpy(np.ones(shape=fakes.shape))
+        minus_pred = torch.subtract(ones, torch.from_numpy(fakes))
+        loss = 1 / m * torch.sum(torch.log2(minus_pred))
         return loss
 
     def set_discriminator(self, discriminator):